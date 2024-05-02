a9f9089c1ef0610c60725752369c502e800b77fb
EdricYeo117 <edricyeo.jr@gmail.com>
2024-05-03 03:01:06 +0800

    Created splash screen to MainActivity Requires tweaking for logo and added branding image

--- app/src/main/java/sg/edu/np/mad/mad24_p02_team1/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/mad24_p02_team1/MainActivity.java
@@ -1,17 +1,33 @@
 package sg.edu.np.mad.mad24_p02_team1;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean isReady = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+        View content = findViewById(android.R.id.content);
+        content.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                if (isReady) {
+                    content.getViewTreeObserver().removeOnPreDrawListener(this);
+                }
+                dismissSplashScreen();
+                return false;
+            }
+        });
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -20,5 +36,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void dismissSplashScreen() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                isReady = true;
+            }
+        }, 3000);
     }
 }
