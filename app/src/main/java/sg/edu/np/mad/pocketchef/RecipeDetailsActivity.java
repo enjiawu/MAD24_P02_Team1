55040d57f3f2dd980bba112e1c4ca1752616a34a
EdricYeo117 <edricyeo.jr@gmail.com>
2024-05-12 23:42:02 +0800

    Implemented 1st Page for Feature:
    Calling random recipe API to list in recycler view

---
+++ app/src/main/java/sg/edu/np/mad/pocketchef/RecipeDetailsActivity.java
@@ -0,0 +1,24 @@
+package sg.edu.np.mad.pocketchef;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.text.MessageFormat;
+import java.util.List;
+
+public class RecipeDetailsActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/sg/edu/np/mad/pocketchef/RequestManager.java
@@ -0,0 +1,96 @@
+package sg.edu.np.mad.pocketchef;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+import sg.edu.np.mad.pocketchef.Listener.RdmRecipeRespListener;
+import sg.edu.np.mad.pocketchef.Listener.RecipeDetailsListener;
+import sg.edu.np.mad.pocketchef.Models.InstructionsResponse;
+import sg.edu.np.mad.pocketchef.Models.RandomRecipeApiResponse;
+import sg.edu.np.mad.pocketchef.Models.RecipeDetailsResponse;
+
+public class RequestManager {
+    Context context;
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://api.spoonacular.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    public RequestManager(Context context) {
+        this.context = context;
+    }
+
+    // Method to call API for random recipes
+    public void getRandomRecipes(RdmRecipeRespListener listener, List<String> tags) {
+        CallRandomRecipes callRandomRecipes = retrofit.create(CallRandomRecipes.class);
+        Call<RandomRecipeApiResponse> call = callRandomRecipes.callRandomRecipe(context.getString(R.string.api_key), "10", tags);
+        call.enqueue(new Callback<RandomRecipeApiResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<RandomRecipeApiResponse> call, @NonNull Response<RandomRecipeApiResponse> response) {
+                if (!response.isSuccessful()) {
+                    listener.didError(response.message());
+                    return;
+                }
+                listener.didFetch(response.body(), response.message());
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<RandomRecipeApiResponse> call, @NonNull Throwable throwable) {
+                listener.didError(throwable.getMessage());
+            }
+        });
+    }
+
+    // Method to call API for recipe information
+    public void getRecipeDetails(RecipeDetailsListener listener, int id) {
+        CallRecipeDetails callRecipeDetails = retrofit.create(CallRecipeDetails.class);
+        Call<RecipeDetailsResponse> call = callRecipeDetails.callRecipeDetails(id, context.getString(R.string.api_key));
+        call.enqueue(new Callback<RecipeDetailsResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<RecipeDetailsResponse> call, @NonNull Response<RecipeDetailsResponse> response) {
+                if (!response.isSuccessful()) {
+                    listener.didError(response.message());
+                    return;
+                }
+                listener.didFetch(response.body(), response.message());
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<RecipeDetailsResponse> call, @NonNull Throwable throwable) {
+                listener.didError(throwable.getMessage());
+            }
+        });
+    }
+
+    // Method to GET random recipes from API
+    private interface CallRandomRecipes {
+        @GET("recipes/random")
+        Call<RandomRecipeApiResponse> callRandomRecipe(
+                @Query("apiKey") String apiKey,
+                @Query("number") String number,
+                @Query("tags") List<String> tags
+        );
+    }
+
+    // Method to GET recipe details from API, based on ID of recipe
+    private interface CallRecipeDetails {
+        @GET("recipes/{id}/information")
+        Call<RecipeDetailsResponse> callRecipeDetails(
+                @Path("id") int id,
+                @Query("apiKey") String apiKey
+        );
+    }
+}
+
