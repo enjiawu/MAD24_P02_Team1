8c2515bcb8e6470da5d45dafe1453417fd655f40
Timothy Chai <s10256132@connect.np.edu.sg>
2024-07-24 00:31:35 +0800

    Add functions, model and listener to pull recipes from API
    Add new activity for loading recipes

---
+++ app/src/main/java/sg/edu/np/mad/pocketchef/Adapters/PantryRecipeAdapter.java
@@ -0,0 +1,22 @@
+package sg.edu.np.mad.pocketchef.Adapters;
+
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PantryRecipeAdapter extends RecyclerView.Adapter<PantryRecipeViewHolder> {
+
+    public PantryRecipeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+
+    public void onBindViewHolder(PantryRecipeViewHolder holder, int position) {
+
+    }
+
+
+    public int getItemCount() {
+        return 0;
+    }
+}
--- app/src/main/java/sg/edu/np/mad/pocketchef/PantryActivity.java
+++ app/src/main/java/sg/edu/np/mad/pocketchef/PantryActivity.java
@@ -1,7 +1,9 @@
 package sg.edu.np.mad.pocketchef;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +14,16 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+
 import java.util.ArrayList;
 
 import sg.edu.np.mad.pocketchef.Adapters.PantryIngredientAdapter;
 
 public class PantryActivity extends AppCompatActivity {
+
+    RecyclerView pantryRecyclerView;
+    ExtendedFloatingActionButton availableRecipesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +36,15 @@ public class PantryActivity extends AppCompatActivity {
             return insets;
         });
 
+        FindViews();
+        SetUpListeners();
+
         ArrayList<String> ingredientList = new ArrayList<>();
         ingredientList.add("Salt");
         ingredientList.add("Sugar");
         ingredientList.add("Butter");
         ingredientList.add("Honey");
 
-        RecyclerView pantryRecyclerView = findViewById(R.id.pantryRecyclerView);
         PantryIngredientAdapter pantryAdapter = new PantryIngredientAdapter(ingredientList);
 
         LinearLayoutManager pantryLayoutManager = new LinearLayoutManager(this);
@@ -43,5 +52,20 @@ public class PantryActivity extends AppCompatActivity {
         pantryRecyclerView.setLayoutManager(pantryLayoutManager);
         pantryRecyclerView.setItemAnimator(new DefaultItemAnimator());
         pantryRecyclerView.setAdapter(pantryAdapter);
+    }
+
+    private void FindViews() {
+        pantryRecyclerView = findViewById(R.id.pantryRecyclerView);
+        availableRecipesButton = findViewById(R.id.availableRecipesButton);
+    }
+
+    private void SetUpListeners() {
+        availableRecipesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent availableRecipes = new Intent(PantryActivity.this, PantryRecipesActivity.class);
+                startActivity(availableRecipes);
+            }
+        });
     }
 }
